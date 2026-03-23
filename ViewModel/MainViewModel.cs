8dea72006b5046de36b0543dfd7880b781c0fb9a
nadestiqJD <maciek.nowak.678@gmail.com>
2026-03-23 15:16:47 +0100

    add project dependency ViewModel to View, RelayCommand implementation with parameter

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private int _ballCount = 10;
+
+        private string _message;
+
+        public int BallCount
+        {
+            get => _ballCount;
+            set
+            {
+                if (_ballCount != value)
+                {
+                    _ballCount = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Message
+        {
+            get => _message;
+            set
+            {
+                _message = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand StartCommand { get; }
+
+        public MainViewModel()
+        {
+            StartCommand = new RelayCommand<object>(_ => StartSimulation());
+        }
+
+        private void StartSimulation()
+        {
+            Message = $"Uruchamiam symulację dla {BallCount} kul.";
+            Console.WriteLine(Message);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
